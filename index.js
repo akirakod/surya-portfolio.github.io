74ee65d95000ed3cf0d0b0364322d2d1a63e28fd
Surya Yadavilli <58390784+akirakod@users.noreply.github.com>
2024-11-10 22:50:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const thumbnails = document.querySelectorAll('.work-item a'); // Select all anchor tags within the work-item articles
+    // Select all anchor tags that wrap the images
+    const thumbnails = document.querySelectorAll('.work-item a');
 
     thumbnails.forEach(thumbnail => {
-        // Add a click event to open the link when the image or the "view" button is clicked
+        // When the user clicks anywhere inside the anchor tag (image or text), the link should open in a new tab
         thumbnail.addEventListener('click', function(event) {
+            // Get the link from the anchor tag
             const url = thumbnail.getAttribute('href');
             if (url) {
                 // Open the link in a new tab
                 window.open(url, '_blank');
             }
-            // Prevent the default behavior of clicking the anchor
-            event.preventDefault(); 
         });
     });
 });
